1b93bd8dab1be8c026af0ea42ce699f714f744bd
SlawomirDr <drachal@tlen.pl>
2022-05-04 13:31:11 +0200

    All functionality for blog webpage is working. Adding, modifing and delating posts

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,39 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, session, flash, redirect, url_for
 from app import app
 from app.models import Entry, db
-from app.forms import EntryForm
+from app.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,6 +42,7 @@ def index():
 
 
 @app.route("/new-post/", methods=["GET", "POST"])
+@login_required
 def create_entry():
    form = EntryForm()
    errors = None
@@ -27,6 +60,7 @@ def create_entry():
    return render_template("entry_form.html", form=form, errors=errors)
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def edit_entry(entry_id):
    entry = Entry.query.filter_by(id=entry_id).first_or_404()
    form = EntryForm(obj=entry)
@@ -38,3 +72,21 @@ def edit_entry(entry_id):
        else:
            errors = form.errors
    return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/delete-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
+def delete_entry(entry_id):
+   errors = None
+   if entry_id: 
+       entry = Entry.query.filter_by(id=entry_id).first_or_404()
+       if request.method == 'POST':
+           db.session.delete(entry)
+           db.session.commit()
+       flash('Post usunięty')
+       return redirect(url_for('index'))
+
+@app.route("/drafts/", methods=["GET", "POST"])
+@login_required
+def list_drafts():
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   return render_template("drafts.html", drafts=drafts)
--- config.py
+++ config.py
@@ -2,7 +2,11 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
 
+
+
 class Config:
+   ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+   ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "admin")
    SECRET_KEY = os.environ.get("SECRET_KEY") or "bloger"  
    SQLALCHEMY_DATABASE_URI = (                        
            os.environ.get('DATABASE_URL') or
